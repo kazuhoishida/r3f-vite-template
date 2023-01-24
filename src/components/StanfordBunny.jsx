db7d26905b78d58e6487eefae9b9b5644ea653f2
Kazuho Ishida <k10700110@gmail.com>
2023-01-25 08:05:56 +0900

    update tsconfig to allow jsx

--- src/components/StanfordBunny.jsx
+++ src/components/StanfordBunny.jsx
@@ -1,10 +1,9 @@
 import { useRef } from "react"
 import { useGLTF } from "@react-three/drei"
-import * as THREE from "three"
 
 export function StanfordBunny({ ...props }) {
-  const group = useRef<THREE.Group>(null)
-  const { nodes, materials }: any = useGLTF("/models/stanford_bunny.glb")
+  const group = useRef(null)
+  const { nodes, materials } = useGLTF("/models/stanford_bunny.glb")
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
